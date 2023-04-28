4e46a7b234469f2a0b9790f69739a87ac86c56ed
Cl1nk404 <92911779+Cl1nk404@users.noreply.github.com>
2023-04-28 15:46:36 +0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -9,9 +9,26 @@ window.onscroll = () =>{
     navbar.classList.remove("active");
 }
 
-document.querySelector("input[type='Number']").forEach(inputNumber =>{
-    inputNumbe.oniput = () =>{
+document.querySelectorAll('input[type="number"]').forEach(inputNumber => { inputNumber.oniput = () =>{
         if(inputNumber.value.length > inputNumber.maxLength) inputNumber.value = inputNumber.value.slice(0, inputNumber.maxLength);
     };
+
+});
+
+
+// Регистрация
+
+const logregBox = document.querySelector('.logreg-box');
+const loginLink = document.querySelector('.login-link');
+const registreLink = document.querySelector('.register-link');
+
+registreLink.addEventListener('click', () => {
+    logregBox.classList.add('active');
+    console.log("1");
+});
+
+loginLink.addEventListener('click', () => {
+    logregBox.classList.remove('active');
+    console.log("2");
 
 });
